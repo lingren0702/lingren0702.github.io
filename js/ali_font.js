91285d8280f36594190d6b4d5daed025f18a3d37
lingren0702 <lingren0702@126.com>
2023-11-26 03:33:58 +0800

    Site updated: 2023-11-26 03:33:58

--- js/ali_font.js
+++ js/ali_font.js
@@ -1,4 +1,4 @@
-!(function (c) {
+<link rel="stylesheet" class="aplayer-secondary-style-marker" href="/assets/css/APlayer.min.css"><script src="/assets/js/APlayer.min.js" class="aplayer-secondary-script-marker"></script>!(function (c) {
     var l,
       h,
       a,
